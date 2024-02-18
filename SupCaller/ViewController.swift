c455e6f39dc0681b3e73bb01aefed6117067599e
smalldevcloud <darkstormcloud8@gmail.com>
2024-02-18 09:08:08 +0300

    Initial Commit

---
+++ SupCaller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SupCaller
+//
+//  Created by 8 on 18.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
